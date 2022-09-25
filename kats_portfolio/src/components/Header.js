c09a30b9c32be3af133bc74bbffbff3510051e02
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-09-24 21:11:32 -0400

    modification

--- kats_portfolio/src/components/Header.js
+++ kats_portfolio/src/components/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg type="lines" bg={true} />
+      <ParticlesBg type="ball" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
